29e482fa4009b4d1e724916722f30a95657792ac
Janik von Rotz <login@janikvonrotz.ch>
2017-03-08 18:32:14 +0100

    add ldap test script

---
+++ .ldapTest.js
@@ -0,0 +1,49 @@
+let ldap = require('ldapjs')
+let assert = require('assert')
+let client = ldap.createClient({
+    url: 'ldap://192.168.90.240'
+})
+
+// http://stackoverflow.com/questions/13255389/ldapjs-authentification-user-login-setup
+
+let options = {
+  filter: '(userPrincipalName=tester@vbl.ch)',
+  scope: 'sub'
+}
+
+let searchUser = {
+  DN: 'CN=Fz vernetztest,OU=Test,OU=Services,OU=vblusers2,DC=vbl,DC=ch',
+  Password: 'ZeNk609Om.',
+}
+
+client.bind(searchUser.DN, searchUser.Password, function(err) {
+  assert.ifError(err)
+  console.info('Bind Success!')
+
+  client.search('dc=vbl,dc=ch', options, function(error, response) {
+    assert.ifError(error)
+
+    response.on('searchEntry', function(entry) {
+      console.info('entry: ' + entry.dn)
+
+      client.bind(entry.dn, 'Password1234', function(bindError) {
+        assert.ifError(bindError)
+        console.info('Login Success!')
+      })
+
+    })
+
+    response.on('searchReference', function(referral) {
+      console.info('referral: ' + referral.uris.join())
+    })
+
+    response.on('error', function(searchError) {
+      console.error('error: ' + searchError.message)
+    })
+
+    response.on('end', function(result) {
+      console.info('status: ' + result.status)
+    })
+
+  })
+})
