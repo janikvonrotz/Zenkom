e234ea5bac6ca578b2931b834083dce4aec2ac09
Janik von Rotz <login@janikvonrotz.ch>
2017-03-13 21:38:17 +0100

    fix update profile method

--- server/methods/users.js
+++ server/methods/users.js
@@ -2,13 +2,14 @@ import { Meteor } from 'meteor/meteor'
 import { Accounts } from 'meteor/accounts-base'
 
 export default () => {
-    
+
   Meteor.methods({
     'users.send_email_verification'() {
       return Accounts.sendVerificationEmail(Meteor.userId())
     },
 
     'users.update_profile'(profile) {
+      profile.name = `${profile.firstname} ${profile.lastname}`
       Meteor.users.update( { _id: Meteor.userId() }, { $set: { profile: profile } } )
     },
   })
