e4ffd2b44b365d3deb7f97b4aae5defd7230d9b7
Janik von Rotz <login@janikvonrotz.ch>
2017-03-14 18:05:16 +0100

    fix import order

--- imports/helpers/index.js
+++ imports/helpers/index.js
@@ -1,4 +1,5 @@
 import getFullname from './getFullname'
+import objectAssign from './objectAssign'
 import config from './config'
 
-export { getFullname, config }
+export { getFullname, config, objectAssign }
