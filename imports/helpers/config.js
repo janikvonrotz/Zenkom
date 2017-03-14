dff99df83b0dfca302295f983257704f1f9b5770
Janik von Rotz <login@janikvonrotz.ch>
2017-03-14 14:50:41 +0100

    ini access control list

--- imports/helpers/config.js
+++ imports/helpers/config.js
@@ -1,3 +1,12 @@
 import { Meteor } from 'meteor/meteor'
 
-export default Object.assign(Meteor.settings.private, Meteor.settings.public)
+let acl = {
+  'routers.read': [ 'tech', 'spec', 'admin' ],
+  'routers.insert': [ 'tech', 'spec', 'admin' ],
+  'routers.update': [ 'tech', 'spec', 'admin' ],
+  'routers.remove': [ 'tech', 'spec', 'admin' ],
+  'routers.restore': [ 'spec', 'admin' ],
+  'routers.get_statistic_url': [ 'tech', 'spec', 'admin' ],
+}
+
+export default Object.assign(Meteor.settings.private, Meteor.settings.public, { acl: acl })
