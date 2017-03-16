0bdcd01b2e5da5b287cc4bd8fd5298c5423931a3
Janik von Rotz <login@janikvonrotz.ch>
2017-03-16 12:23:05 +0100

    update flexbox

--- client/core/FlexboxGrid.js
+++ client/core/FlexboxGrid.js
@@ -4,13 +4,13 @@ import { Row, Col, BoxRow } from '../flexboxgrid'
 
 let FlexboxGrid = (props) => {
   return <Row>
-    <Col xs="0" sm="2" md="2" lg="1">
+    <Col xs="0" sm="2" md="2" lg="2">
       <BoxRow />
     </Col>
-    <Col xs="12" sm="8" md="8" lg="10">
+    <Col xs="12" sm="8" md="8" lg="8">
       <BoxRow>{ props.children }</BoxRow>
     </Col>
-    <Col xs="0" sm="2" md="2" lg="1">
+    <Col xs="0" sm="2" md="2" lg="2">
       <BoxRow />
     </Col>
   </Row>
