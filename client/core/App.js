adb90576df9407961366b586a8377455a32b25dd
Janik von Rotz <login@janikvonrotz.ch>
2017-03-27 08:57:21 +0200

    reset limit on entry page

--- client/core/App.js
+++ client/core/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card, CardText, CardMedia } from 'material-ui'
-import { setHeaderTitle } from '../actions'
+import { setHeaderTitle, resetListLimit } from '../actions'
 import { connect } from 'react-redux'
 import { NotificationList } from '../notifications'
 import { isAllowed } from '/imports/helpers'
@@ -8,8 +8,11 @@ import { isAllowed } from '/imports/helpers'
 class App extends React.Component {
 
   componentDidMount(){
+    let { dispatch } = this.props
+    dispatch(resetListLimit())
     this.componentWillReceiveProps()
   }
+  
   componentWillReceiveProps(){
     let { dispatch, i18n } = this.props
     dispatch(setHeaderTitle(i18n.button.dashboard))
