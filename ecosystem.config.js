08428418cf68bd46f4ecd01b94cc699df5af77f4
Janik von Rotz <login@janikvonrotz.ch>
2017-03-31 09:50:32 +0200

    update deployment config

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'Zenkom',
+      script: './bundle/main.js',
+      env: {
+        NODE_ENV: 'dev',
+        MONGO_URL: 'mongodb://zenkom:dp8e36APuASgSWum7uLz@ds121190.mlab.com:21190/zenkom',
+        ROOT_URL: 'https://zenkom.bitsherpa.com',
+        PORT: 3000,
+        METEOR_SETTINGS: {
+          'private': {
+            'ldap': {
+              'url': 'ldap://ldap.forumsys.com',
+              'base': 'dc=example,dc=com',
+              'filter': '(mail={email})',
+              'scope': 'sub',
+            },
+            'mail': {
+              'notificationFrom': 'notifications@zenkom.bitsherpa.com',
+            },
+          },
+          'public': {
+            'disableAccessContol': false,
+          }
+        }
+      },
+      env_prod: {
+      },
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    dev: {
+      user: 'login',
+      host: 'janikvonrotz.ch',
+      key: '/keybase/private/janikvonrotz/ssh/id_rsa',
+      ref: 'origin/develop',
+      repo: 'git@gitlab.com:janikvonrotz/Zenkom.git',
+      path: '/home/login/Zenkom',
+      'post-deploy': 'rm -rf ./bundle && npm install --production && meteor build . --directory && cd ./bundle/programs/server && npm install --production && cd ../../.. && pm2 startOrRestart ecosystem.config.js',
+    }
+  }
+}
