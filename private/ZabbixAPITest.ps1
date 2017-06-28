702d225af87134cab380cf838a32b87951a43b73
Janik von Rotz <login@janikvonrotz.ch>
2017-06-28 19:14:22 +0200

    cleanup

--- private/ZabbixAPITest.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest -Uri "http://www.oev-live.ch/zabbix/api_jsonrpc.php" 
--Method "POST" 
--ContentType "application/json-rpc"
--Body '{"jsonrpc":"2.0","method":"apiinfo.version","id":1,"auth":null,"params":{}}'
